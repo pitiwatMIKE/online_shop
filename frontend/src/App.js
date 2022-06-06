473a43c7e1fc4e11e4cbbe10ad5f672169ce1c15
pitiwatMIKE <pitiwat.pr61@gmail.com>
2022-06-06 13:37:39 +0700

    install boostrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,7 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-export default function App() {
-  
-  useEffect(() => {
-    (async () => {
-      const reposne = await axios.get("/server");
-      console.log(reposne.data);
-    })();
-  }, []);
+import React from 'react'
 
+export default function App() {
   return (
-    <>
-      <h1> App </h1>
-    </>
-  );
+    <div>App</div>
+  )
 }

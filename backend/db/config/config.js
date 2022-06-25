856e3438660fbe4a5b8269bce03843faf02c3b30
pitiwatMIKE <pitiwat.pr61@gmail.com>
2022-06-25 17:02:43 +0700

    prepare deploy

--- backend/db/config/config.js
+++ backend/db/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -17,21 +17,19 @@ module.exports = {
     password: process.env.CI_DB_PASSWORD,
     database: process.env.CI_DB_NAME,
     host: "127.0.0.1",
-    port: 3306,
-    dialect: "mysql",
+    port: 5432,
+    dialect: "postgres",
     dialectOptions: {
       bigNumberStrings: true,
     },
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
-    dialect: "mysql",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
     dialectOptions: {
-      bigNumberStrings: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 };
